80e9cfb115a4ad0e11cbe84a782846b3eb79ffcb
raebaexxx <vadimfeda@yandex.ru>
2026-05-19 16:21:15 +0300

    increase aiohttp timeout to 1 hour for large file uploads
    1.6GB files need more than default 5min timeout

--- bot/main.py
+++ bot/main.py
@@ -9,6 +9,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram.client.telegram import TelegramAPIServer
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiohttp import ClientTimeout
 
 from config import BOT_TOKEN, BOT_API_SERVER_URL, LOG_LEVEL
 from database.models import init_db
@@ -21,16 +22,18 @@ logging.basicConfig(
 
 storage = MemoryStorage()
 
+LONG_TIMEOUT = ClientTimeout(total=3600, connect=30, sock_read=3600)
+
 
 async def main():
     await init_db()
 
     if BOT_API_SERVER_URL:
         local_server = TelegramAPIServer.from_base(BOT_API_SERVER_URL)
-        session = AiohttpSession(api=local_server)
+        session = AiohttpSession(api=local_server, timeout=LONG_TIMEOUT)
         logging.info(f"Using local Bot API Server: {BOT_API_SERVER_URL}")
     else:
-        session = AiohttpSession()
+        session = AiohttpSession(timeout=LONG_TIMEOUT)
 
     bot = Bot(token=BOT_TOKEN, session=session)
     dp = Dispatcher(storage=storage)
